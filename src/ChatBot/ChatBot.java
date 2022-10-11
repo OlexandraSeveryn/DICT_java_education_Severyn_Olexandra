4d421d9aa94b02a625b1f7edbeda156c889d1361
OlexandraSeveryn <o.y.severyn@student.khai.edu>
2022-10-11 15:15:57 +0300

    ChatBot 1-st stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,10 @@
+package ChatBot;
+
+public class ChatBot {
+    public static void main (String[] args){
+        String name = "Sunrise";
+        int creatingData = 2022;
+        System.out.println("Hello my name is " + name);
+        System.out.println("I was created in " + creatingData);
+    }
+}
