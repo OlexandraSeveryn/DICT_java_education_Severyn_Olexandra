30b649a84dc9a90eb104f9246d178a6ca589d876
OlexandraSeveryn <o.y.severyn@student.khai.edu>
2022-12-17 23:05:12 +0200

    "hangman: 5-th stage"

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,23 +1,44 @@
 import java.util.*;
 
 public class Hangman {
-    public static void main(String[] args){
-        System.out.print("HANGMAN\nGuess the word ");
+    public static void main(String[] args) {
+        System.out.println("HANGMAN");
         String[] guessingWords = {"javascript", "java", "python", "kotlin"};
         Scanner scanner = new Scanner(System.in);
         Random randWord = new Random();
+        ArrayList<String> letters = new ArrayList<>();
         String word = guessingWords[randWord.nextInt(guessingWords.length)];
+        byte attempts = 8;
+        boolean True = true;
 
+        StringBuilder hint = new StringBuilder(word);
         char[] hide = word.toCharArray();
-        for (int i = 2; i < hide.length; i++)
-            hide[i] = '-';
-        String hint = String.valueOf(hide);
-        System.out.print(hint + ": ");
-        String guessedWord = scanner.next();
-        if (guessedWord.equals(word)) {
-            System.out.println("You survived!");
-        } else {
-            System.out.println("You lost!");
+        for (int i = 0; i < hide.length; i++)
+            hint.setCharAt(i, '-');
+
+        while (True) {
+            if (attempts > 0) {
+                System.out.print(hint + "\nInput a letter: ");
+                String guessedWord = scanner.next();
+                letters.add(guessedWord);
+                attempts--;
+                if (word.contains(guessedWord)) {
+                    for (int i = 0; i < word.length(); i++) {
+                        if (hide[i] == guessedWord.charAt(0)) {
+                            hint.setCharAt(i, guessedWord.charAt(0));
+                        }
+                    }
+                    if (guessedWord.equals(word)) {
+                        System.out.println("Thanks for playing!\nWe'll see how well you did in the next stage");
+                        break;
+                    }
+                }else {
+                    System.out.println("That letter doesn't appear in the word");
+                }
+            } else {
+                System.out.println("Thanks for playing!\nWe'll see how well you did in the next stage");
+                break;
+            }
         }
     }
 }
