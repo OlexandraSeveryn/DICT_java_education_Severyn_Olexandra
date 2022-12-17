526ebf4689f686eab4ec1a03af32e00716c8aa0b
OlexandraSeveryn <o.y.severyn@student.khai.edu>
2022-12-17 20:13:10 +0200

    "hangman: 2-nd stage"

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,5 +1,16 @@
+import java.util.*;
+
 public class Hangman {
     public static void main(String[] args){
-        System.out.println("HANGMAN\nThe game will be available soon.");
+        System.out.print("HANGMAN\nGuess the word: ");
+        String word = "javascript";
+        Scanner scanner = new Scanner(System.in);
+        String guessedWord = scanner.next();
+
+        if (guessedWord.equals(word)) {
+            System.out.println("You survived!");
+        } else {
+            System.out.println("You lost!");
+        }
     }
 }
